8e838ba2f2ae0f6c7f726f510153d4f5bb058a10
Francisco <francisco.raverta@gmail.com>
2025-06-10 19:37:10 -0300

    Add covariance computation

--- opensfm/src/bundle/data/data.h
+++ opensfm/src/bundle/data/data.h
@@ -63,7 +63,7 @@ struct Data : public DataNode {
     if (!HasCovariance()) {
       throw std::runtime_error(GetID() + " hasn't any covariance");
     } else {
-      std::cout << "Covariance Matrix: " << covariance_ << endl;
+      std::cout << "Covariance Matrix: " << covariance_.Value() << std::endl;
     }
 
     return covariance_.Value();
