85edd3e7e3ac48aa3483e7857108db3e768d4a1d
Francisco <francisco.raverta@gmail.com>
2025-08-06 19:12:29 -0300

    Add logger for matching outlier rejection

--- opensfm/matching.py
+++ opensfm/matching.py
@@ -46,7 +46,7 @@ def setup_segmentation_logger(log_file='volume/matching_filter.txt'):
     return logger
 
 #logger_matches = setup_segmentation_logger()
-logger_matches: logging.Logger = logging.getLogger("matches.txt")
+logger_matches: logging.Logger = logging.getLogger("matches_segmentation.json")
 
 def clear_cache() -> None:
     feature_loader.instance.clear_cache()
