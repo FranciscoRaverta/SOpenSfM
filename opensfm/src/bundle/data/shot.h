c59d92f5bb0cd5fd75e53a599b8cf6eb5b5c316b
FranciscoRaverta <francisco.raverta@gmail.com>
2026-02-02 12:51:23 +0100

    Added image segmentation to Shot structure

--- opensfm/src/bundle/data/shot.h
+++ opensfm/src/bundle/data/shot.h
@@ -49,7 +49,7 @@ struct Shot : public DataContainer {
     return static_cast<RigInstance *>(GetData("rig_instance"));
   }
 
-  const MatXd& GetCovariance() {
+  const MatXd& GetCovariance() const {
     return static_cast<RigInstance *>(GetData("rig_instance"))->GetCovariance();
   }
 
