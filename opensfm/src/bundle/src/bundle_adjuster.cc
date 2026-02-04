9a9a2b72e6ae4a1e45de8bc7d15f45d50b913aa8
FranciscoRaverta <francisco.raverta@gmail.com>
2026-02-04 17:03:51 +0100

    Added covariance to the opensmv exporter

--- opensfm/src/bundle/src/bundle_adjuster.cc
+++ opensfm/src/bundle/src/bundle_adjuster.cc
@@ -1358,12 +1358,12 @@ void BundleAdjuster::ComputeCovariances(ceres::Problem *problem) {
       i.second.GetRigInstance()->SetCovariance(default_covariance_matrix);
     }
   }
-  // std::cout << "FRAN - Covariances computed: " << computed << std::endl;
-  // for (auto &i : shots_) {
-  //     std::cout << "FRAN - Shot id: " << i.second.GetID() << std::endl;
-  //     std::cout << "FRAN - Covariance Matrix: " << std::endl;
-  //     std::cout << i.second.GetRigInstance()->GetCovariance() << std::endl;
-  //   }
+  std::cout << "FRAN - Covariances computed: " << computed << std::endl;
+  for (auto &i : shots_) {
+      std::cout << "FRAN - Shot id: " << i.second.GetID() << std::endl;
+      std::cout << "FRAN - Covariance Matrix: " << std::endl;
+      std::cout << i.second.GetRigInstance()->GetCovariance() << std::endl;
+    }
 }
 
 void BundleAdjuster::ComputeReprojectionErrors() {
