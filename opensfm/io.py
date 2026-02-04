07e80a7c01b9db8ecc2d137f0b024c23da09b117
FranciscoRaverta <francisco.raverta@gmail.com>
2026-02-04 17:42:47 +0100

    Added covariance to the opensmv exporter

--- opensfm/io.py
+++ opensfm/io.py
@@ -161,9 +161,10 @@ def assign_shot_attributes(obj: Dict[str, Any], shot: pymap.Shot) -> None:
         shot.scale = obj["scale"]
     if "covariance" in obj:
         shot.covariance = np.array(obj["covariance"])
-        print(f"FRAN1 - {(obj["covariance"])}")
-        print(f"FRAN2 - {np.array(obj["covariance"])}")
-        print(f"FRAN3 - {shot.covariance}")
+        print("FRAN")
+        print(obj["covariance"])
+        print(np.array(obj["covariance"]))
+        print(shot.covariance)
     if "merge_cc" in obj:
         shot.merge_cc = obj["merge_cc"]
     if "vertices" in obj and "faces" in obj:
